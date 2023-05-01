20ed61481b2c92e4f931f93e7e7ccc057e762f0f
Jackson Carmo <jacksoncruciol157@gmail.com>
2023-04-30 22:46:31 -0300

    configuracao do cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
 const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+})
 
 app.get('/', function (req, res) {
   res.send('Bem Vindo ao chat');
@@ -9,7 +19,7 @@ const server = app.listen(8080, ()=> {
     console.log("Servidor iniciado na porta 8080:")
 });
 
-io = socket(server);
+io = socket(server, {cors: {origin: "*"}});
 
 io.on("connection", (socket) => {
     console.log(socket.id);
