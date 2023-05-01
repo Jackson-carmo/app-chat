735d157f3f5391f93ac4a1855e85a4db78788d21
Jackson Carmo <jacksoncruciol157@gmail.com>
2023-04-30 22:32:53 -0300

    connection socket

--- back-end/app.js
+++ back-end/app.js
@@ -9,3 +9,8 @@ const server = app.listen(8080, ()=> {
     console.log("Servidor iniciado na porta 8080:")
 });
 
+io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+});
